a8a7476d5d626a852153439f0367f691342c3240
Christian Halvorsen <Christianhalvorsen1998@gmail.com>
2019-10-25 17:14:37 +0200

    Application now works!

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+      <form @submit='addTodo'>
+          <input type="text" v-model="title" name="title" placeholder="Add Todo">
+          <input type="submit" value="Submit" class="btn">
+      </form>
+  </div>
+</template>
+
+<script>
+// Lets u generate a random unique ID
+import uuid from 'uuid';
+
+export default {
+    name: 'AddTodo',
+    data(){
+        return {
+            title: '' 
+        }
+    },
+    methods: {
+        addTodo(evt){
+            evt.preventDefault();
+            const newTodo = {
+                id: uuid.v4(),
+                title: this.title,
+                completed: false
+            }
+            // send up to parent
+            this.$emit('add-todo', newTodo);
+            // reset the input field
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form{
+        display: flex;
+    }
+
+    input[type='text']{
+        flex: 10;
+        padding: 10px;
+        font-size: 1em;
+    }
+
+    input[type='submit']{
+        flex: 2;
+        font-size: 1em;
+    }
+</style>
